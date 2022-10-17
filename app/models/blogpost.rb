5d9a86f6f9e415e0258b80084d0b1de5adcc8c8a
Urwah Waris <urwah.wrs17@gmail.com>
2022-10-17 19:33:40 +0500

    final-03

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -2,7 +2,7 @@ class Blogpost < ApplicationRecord
   belongs_to :blogger
   has_many :comments, dependent: :destroy
   has_one_attached :image
-  validates :image, presence: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a GIF, JPG or PNG image.'}
+  validates :image, presence: true, format: { with: %r{\.(gif|jpg|png|jpeg)\Z}i, message: 'must be a GIF, JPG or PNG image.'}
 
  
   has_many :favorited_by, :through => :favorite_blogposts, source: :blogger # the actual users favoriting a recipe
