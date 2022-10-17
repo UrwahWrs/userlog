c3a3145a219d0e08ec54f86e37ae649599291c72
Urwah Waris <urwah.wrs17@gmail.com>
2022-10-17 19:48:55 +0500

    final-03(01)

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -2,10 +2,21 @@ class Blogpost < ApplicationRecord
   belongs_to :blogger
   has_many :comments, dependent: :destroy
   has_one_attached :image
-  validates :image, presence: true, format: { with: %r{\.(gif|jpg|png|jpeg)\Z}i, message: 'must be a GIF, JPG or PNG image.'}
 
+  validate :image_type
+
+private
+
+def image_type
+   if image.attached? == false
+     errors.add(:image, "is missing!")
+   end
+   if !image.content_type.in?(%('image/jpeg image/png image/jpg'))
+     errors.add(:image, "needs to be a jpeg, jpg or png!")
+   end
+end
  
-  has_many :favorited_by, :through => :favorite_blogposts, source: :blogger # the actual users favoriting a recipe
+  has_many :favorited_by, :through => :favorite_blogposts, source: :blogger 
 
 
   validates :title, presence: true,length: { minimum: 5 }
